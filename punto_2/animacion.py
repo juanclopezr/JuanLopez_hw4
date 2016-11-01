87d878c2f0126e5ab709d6f1671a3d814ffe4813
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-10-31 20:25:01 -0500

    scales fixed

--- punto_2/animacion.py
+++ punto_2/animacion.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-for i in range(0,352):
+for i in range(0,400):
     data = np.loadtxt("cuerda%05d.dat" %(i));
     plt.plot(data[:,0],data[:,1])
     plt.ylim([-1,1])
     plt.xlabel('x')
     plt.ylabel('Amplitude')
-    plt.title('t=%d' %(i))
+    plt.title('t=%d' %(i/2))
     plt.savefig('cuerda%05d.png' %(i), format='png')
     plt.close()
 
